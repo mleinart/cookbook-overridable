ca8515912a5b4383fbb4233dce9e054037de244b
Michael Leinartas <mleinartas@gmail.com>
2011-12-02 15:50:05 -0600

    fix mode attribute and add override_path attribute

--- resources/template.rb
+++ resources/template.rb
@@ -14,9 +14,10 @@ attribute :checksum, :regex => /^[a-zA-Z0-9]{64}$/
 attribute :cookbook, :kind_of => String
 attribute :group, :kind_of => String
 attribute :local, :kind_of => [ TrueClass, FalseClass ]
-attribute :mode, :kind_of => String
+attribute :mode
 attribute :only_if_missing, :kind_of => [ TrueClass, FalseClass ], :default => false
 attribute :owner, :kind_of => String
+attribute :override_path, :kind_of => String, :default => "overrides"
 attribute :path, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String, :default => "#{::File.basename(name)}.erb"
 attribute :variables, :kind_of => Hash
