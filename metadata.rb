ec6641f142aaaa71b9b360a705e618ddf137baf1
Michael Leinartas <mleinartas@gmail.com>
2011-12-02 20:43:45 -0600

    rev 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "mleinartas@gmail.com"
 license          "Apache 2.0"
 description      "A wrapper for the Template resource that provides more flexibility to cookbook users"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
